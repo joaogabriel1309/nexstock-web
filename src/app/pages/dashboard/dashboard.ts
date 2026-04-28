2c3327ceca3d7af9c07f1cda1907b11beef03399
João Gabriel de Matos Silva <74660748+joaogabriel1309@users.noreply.github.com>
2026-04-28 08:16:24 -0400

    fix(dashboard): alinhar estoque com nova api

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -18,13 +18,12 @@ import { UsuarioService } from '../../services/usuario.service';
     MatCardModule,
     MatIconModule,
     MatProgressSpinnerModule,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnInit {
-
   carregando = true;
   produtos: ProdutoResponse[] = [];
   usuarios: UsuarioResponse[] = [];
@@ -32,38 +31,31 @@ export class Dashboard implements OnInit {
   constructor(
     private produtoService: ProdutoService,
     private usuarioService: UsuarioService,
-    private tokenService: TokenService
+    private tokenService: TokenService,
   ) {}
 
   ngOnInit(): void {
-  console.log('Dashboard Iniciado');
     const empresaId = this.tokenService.getEmpresaId();
 
-  console.log('ID da Empresa recuperado:', empresaId); 
-
-  if (!empresaId) {
-    console.error('Empresa ID não encontrado! Redirecionando...');
-    this.carregando = false; 
-    return;
-  }
-    if (empresaId) {
-      forkJoin({
-        produtos: this.produtoService.listar(empresaId),
-        usuarios: this.usuarioService.listarTodoPorEmpresa(empresaId)
-      }).subscribe({
-        next: (res) => {
-          this.produtos = res.produtos;
-          this.usuarios = res.usuarios;
-          this.carregando = false;
-        },
-        error: (err) => {
-          console.error('Erro ao carregar dashboard', err);
-          this.carregando = false;
-        }
-      });
-    } else {
+    if (!empresaId) {
       this.carregando = false;
+      return;
     }
+
+    forkJoin({
+      produtos: this.produtoService.listar(empresaId),
+      usuarios: this.usuarioService.listarTodoPorEmpresa(empresaId),
+    }).subscribe({
+      next: (res) => {
+        this.produtos = res.produtos;
+        this.usuarios = res.usuarios;
+        this.carregando = false;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar dashboard', err);
+        this.carregando = false;
+      },
+    });
   }
 
   get totalUsuarios(): number {
@@ -75,10 +67,12 @@ export class Dashboard implements OnInit {
   }
 
   get estoqueTotal(): number {
-    return this.produtos.reduce((acc, p) => acc + (p.estoque || 0), 0);
+    return this.produtos.reduce((acc, p) => acc + (p.estoqueAtual || 0), 0);
   }
 
   get produtosBaixoEstoque(): ProdutoResponse[] {
-    return this.produtos.filter(p => (p.estoque || 0) < 10).slice(0, 5);
+    return this.produtos
+      .filter((p) => p.ativo && (p.estoqueAtual || 0) <= (p.estoqueMinimo || 0))
+      .slice(0, 5);
   }
 }
